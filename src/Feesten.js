3a567a23f71d3de3909d353c4d522342e8e6a2da
MiesvanGogh <93597938+MiesvanGogh@users.noreply.github.com>
2022-10-18 15:11:47 +0200

    feesten pagina mooier gemaakt

--- src/Feesten.js
+++ src/Feesten.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState }  from 'react';
 import Appbar from './Components/Appbar'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 const Feesten = () => {
     const[users, setUsers]=useState([])
+    const paperStyle={padding: '20px 20px', width:400, margin:"20px auto"}
 
     useEffect(()=>{
         fetch("http://localhost:8080/user/getAll")
@@ -13,15 +16,26 @@ const Feesten = () => {
       )
       },[])
 
-    return(
-        <div className="app">
-            <div>
-                 <Appbar />
-            </div>  
-            <div>
-                <h1>Feesten</h1>
-            </div> 
-        </div>
-    )
+      return (
+        <container>
+          <Appbar/> 
+          <Paper elevation={3} style={paperStyle}>
+            <h1>Alle Feesten</h1>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+            <h1> Bas</h1>
+            <h1> Jasper</h1>
+            <h1> Mies</h1>
+            <h1> Yannick</h1>
+        </Box>
+        </Paper>
+        </container>
+      );
 }
 export default Feesten;
