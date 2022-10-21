57f6b6ec67012cea729320729dcabe687e22e766
MiesvanGogh <93597938+MiesvanGogh@users.noreply.github.com>
2022-10-21 14:30:43 +0200

    Calender in feesten page that sends data

--- src/Feesten.js
+++ src/Feesten.js
@@ -1,41 +1,32 @@
 import React, { useEffect, useState }  from 'react';
 import Appbar from './Components/Appbar'
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+import { render } from 'react-dom';
+import Calendar from 'moedim';
 
 const Feesten = () => {
-    const[users, setUsers]=useState([])
-    const paperStyle={padding: '20px 20px', width:400, margin:"20px auto"}
+    const [date, setDate] = useState(new Date());
 
-    useEffect(()=>{
-        fetch("http://localhost:8080/user/getAll")
-        .then(res=>res.json())
-        .then((result)=>{
-          setUsers(result);
-        }
-      )
-      },[])
+    const onChange = date => {
+      setDate(date);
+      console.log(date);
+    };
+
+    // useEffect(()=>{
+    //     fetch("http://localhost:8080/user/getAll")
+    //     .then(res=>res.json())
+    //     .then((result)=>{
+    //       setUsers(result);
+    //     }
+    //   )
+    //   },[])
 
       return (
-        <container>
-          <Appbar/> 
-          <Paper elevation={3} style={paperStyle}>
-            <h1>Alle Feesten</h1>
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1 },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-            <h1> Bas</h1>
-            <h1> Jasper</h1>
-            <h1> Mies</h1>
-            <h1> Yannick</h1>
-        </Box>
-        </Paper>
-        </container>
+        <div>
+          <Appbar/>
+           <div className='b'> 
+            <Calendar onChange={onChange} value={date}/>
+           </div>
+        </div>
       );
 }
 export default Feesten;
