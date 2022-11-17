9a2e17b108e21a7edfcf4774287f919b1a9622db
MiesvanGogh <93597938+MiesvanGogh@users.noreply.github.com>
2022-11-17 11:37:27 +0100

    nice

--- src/Feesten.js
+++ src/Feesten.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }  from 'react';
+import React from 'react';
 import Appbar from './Components/Appbar';
 import Calendar from './Components/Calendar';
 import {Link} from "react-router-dom";
